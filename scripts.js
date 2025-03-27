0af3955d21214d901f65a69002a45a87b0871e66
Rodrigo Martins <mr.rodrigocarrico@gmail.com>
2025-03-27 11:32:58 +0000

    letters removed (input)

--- scripts.js
+++ scripts.js
@@ -2,5 +2,8 @@
 const amount = document.getElementById("amount");
 
 amount.oninput = () =>{
-  console.log("Interação com o input!")
+  // Aqui recebe o valor do input e remove os caracteres não numéricos e devolve os números
+  let value = amount.value.replace(/\D/g, "")
+  // e atribui o valor ao input novamente
+  amount.value = value 
 }
