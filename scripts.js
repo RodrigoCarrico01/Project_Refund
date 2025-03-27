50490de46bec4a8591cadd9c1314ca9c86310280
Rodrigo Martins <mr.rodrigocarrico@gmail.com>
2025-03-27 11:41:17 +0000

    amount input format

--- scripts.js
+++ scripts.js
@@ -1,9 +1,26 @@
 //Seleciona os elementos do formulário
 const amount = document.getElementById("amount");
 
+// Captura o evento de input para formatar o valor.
 amount.oninput = () =>{
-  // Aqui recebe o valor do input e remove os caracteres não numéricos e devolve os números
+  // Aqui recebe o valor atual do input e remove os caracteres não numéricos.
   let value = amount.value.replace(/\D/g, "")
-  // e atribui o valor ao input novamente
-  amount.value = value 
+
+  //Transformar o valor em centávos (exemplo: 150/100 = 1.50 que é equivalente a R$ 1,50).
+  value = Number(value) / 100
+
+  // Atualiza o valor do input com o valor sem letras.
+  amount.value = formatCurrencyBRL(value)
+  
+}
+
+function formatCurrencyBRL(value){
+  //Formatar o valor no padrão BRL (real brasileiro)
+  value = value.toLocaleString('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  })
+
+  //Retorna o valor formatado.
+  return value
 }
